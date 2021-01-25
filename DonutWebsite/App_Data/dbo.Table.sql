862d9742a81ceaf0f47d8915d66e6fc054f4a390
kingaord <kingaordecka@gmail.com>
2021-01-25 21:57:55 +0100

    ContentForm update

---
+++ DonutWebsite/App_Data/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(150) NOT NULL, 
+    [Content] TEXT NOT NULL, 
+    [DateCreated] DATETIME NOT NULL, 
+    [DatePublished] DATETIME NOT NULL, 
+    [AuthorID] INT NOT NULL, 
+    [Slug] NVARCHAR(50) NOT NULL
+)
--- DonutWebsite/ContentForm.aspx.designer.cs
+++ DonutWebsite/ContentForm.aspx.designer.cs
@@ -13,5 +13,59 @@ namespace DonutWebsite
 
     public partial class ContentForm
     {
+
+        /// <summary>
+        /// Kontrolka form2.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
+
+        /// <summary>
+        /// Kontrolka ptitle.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ptitle;
+
+        /// <summary>
+        /// Kontrolka PostTitle.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PostTitle;
+
+        /// <summary>
+        /// Kontrolka pcontent.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label pcontent;
+
+        /// <summary>
+        /// Kontrolka PostContent.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PostContent;
+
+        /// <summary>
+        /// Kontrolka btnPublish.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPublish;
     }
 }
