88ed20eb28a52a8a8bddf544e2b5788740ece18e
kingaord <kingaordecka@gmail.com>
2021-01-30 16:46:17 +0100

    final version

---
+++ DonutWebsite/App_Data/dbo.Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT           NOT NULL,
+    [UserName] VARCHAR (50)  NOT NULL,
+    [Password] VARCHAR (100) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
