19f3795f1d68e0bca1cdafc0b400f67ccb6322a0
kingaord <kingaordecka@gmail.com>
2021-01-30 15:47:13 +0100

    Blog and ContentForm updated

--- DonutWebsite/App_Data/dbo.Posts.sql
+++ DonutWebsite/App_Data/dbo.Posts.sql
@@ -1,7 +1,8 @@
-﻿CREATE TABLE [dbo].[Table] (
+﻿CREATE TABLE [dbo].[Posts] (
     [Id]      INT            IDENTITY (1, 1) NOT NULL,
     [Title]   NVARCHAR (150) NOT NULL,
     [Content] TEXT           NOT NULL,
+    [PostedDate] DATETIME NOT NULL, 
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
